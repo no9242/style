e06d3b6a41ed14b0956a2ced83c30f413bcb7d5a
no9242 <xjren@foxmail.com>
2019-03-06 21:59:53 +0800

    test

---
+++ demo.js
@@ -0,0 +1,8 @@
+onload = function(){
+    let main = window.document.getElementById('main');
+    main.children[0].setAttribute('name','header');
+}
+
+function mainOnclick(e){
+    console.log(e)
+}
